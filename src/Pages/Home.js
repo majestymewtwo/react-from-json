72bcc7ae95e0fb205e433f611efbc3c54e3e4854
JayashreeTJ2004 <shreejai4002@gmail.com>
2023-05-22 15:47:33 +0530

    Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,25 @@
-import data from '../json/configuredata.json'
+import data from "../json/configuredata.json";
 import DynamicComponent from "../components/DynamicComponent";
+import { useState } from "react";
 
 export default function Home() {
-  const pageKey = 'page1';
-  
+  const pageKey = "page1";
+  const [mobile, setMobile] = useState("");
+  const onclick1 = {
+    alert: () => {
+      alert("Hello! I am an alert box!");
+    },
+    alert2: () => {
+      alert("Alert2 Alert2 Alert2");
+    },
+    mobile:mobile,
+    handleMobile:(event)=>{
+      setMobile(event.target.value);
+    }
+  };
   return (
     <div>
-      <DynamicComponent config={data} pageKey={pageKey}  />
+      <DynamicComponent config={data} pageKey={pageKey} onclick1={onclick1} />
     </div>
   );
 }
--- src/components/DynamicComponent.js
+++ src/components/DynamicComponent.js
@@ -1,44 +1,22 @@
-import React,{useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import "../css/configurecss.css";
-import amazonlogo from "../assets/amazonlogo.png"
-import {MyContext} from "../context/inputCopy"
-
+import amazonlogo from "../assets/amazonlogo.png";
+import { MyContext } from "../context/inputCopy";
 import { Link } from "react-router-dom";
+const DynamicComponent = ({ config, pageKey, onclick1 }) => {
+  const { val } = useContext(MyContext);
+  const [inputValue, setInputValue] = val;
+  const [inputValue1, setInputValue1] = useState(" ");
 
-const onclick1 = {
-  alert: () => {
-    alert("Hello! I am an alert box!");
-  },
-  alert2: () => {
-    alert("Alert2 Alert2 Alert2");
-  },
-};
-
-const DynamicComponent = ({ config, pageKey }) => {
-  const {val}=useContext(MyContext); 
-  const [inputValue,setInputValue]=val;
-  const [inputValue1,setInputValue1]=useState(' ');
-  const inp={
-    inputValue,
-    inputValue1
-  }
-  const setVal={
-    handleChange:(event)=>{
-  setInputValue(event.target.value);
-    },
-    handleChange1:(event)=>{
-      setInputValue1(event.target.value);
-        },
-    
-  };
   const renderComponent = (component) => {
     const {
       tag,
-      content,
+      content,  
       props,
       children,
-     contentcopy,
+      contentcopy,
       value,
+      variable,
       onChange,
       onClick,
       pageKey: componentPageKey,
@@ -48,27 +26,38 @@ const DynamicComponent = ({ config, pageKey }) => {
     if (componentPageKey && componentPageKey !== pageKey) {
       return null;
     }
-   
-if(ComponentTag=="input"||ComponentTag == "img" || ComponentTag=='hr'){
-  return <ComponentTag {...props}  value={inp[value]} onChange={setVal[onChange]}  onClick={onclick1[onClick]} />;
-}
-    if(ComponentTag =="Link"){
-      return <Link {...props}>{content}</Link>
+
+    if (
+      ComponentTag == "input" ||
+      ComponentTag == "img" ||
+      ComponentTag == "hr"
+    ) {
+      return (
+        <ComponentTag
+          {...props}
+          value={onclick1[value]}
+          onChange={onclick1[onChange]}
+          onClick={onclick1[onClick]}
+        />
+      );
+    }
+    if (ComponentTag == "Link") {
+      return <Link {...props}>{content}</Link>;
     }
 
-if(contentcopy){
-  return (
-    <ComponentTag {...props} onClick={onclick1[onClick]}>
-      {inputValue}
-   
-      {children && children.map(renderComponent)}
-    </ComponentTag>
-  );
-}
+    if (contentcopy) {
+      return (
+        <ComponentTag {...props} onClick={onclick1[onClick]}>
+          {inputValue}
+
+          {children && children.map(renderComponent)}
+        </ComponentTag>
+      );
+    }
     return (
       <ComponentTag {...props} onClick={onclick1[onClick]}>
         {content}
-     
+        {onclick1[variable]}
         {children && children.map(renderComponent)}
       </ComponentTag>
     );
